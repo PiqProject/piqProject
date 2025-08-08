685284f192f32c98caa07d68648cdf2f003a0adb
ttkakrnl33 <ttkakrnl33@gmail,com>
2025-08-09 00:31:01 +0900

    first

---
+++ src/main/java/piq/piqproject/PiqProjectApplication.java
@@ -0,0 +1,13 @@
+package piq.piqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiqProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiqProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/piq/piqproject/PiqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package piq.piqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiqProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
