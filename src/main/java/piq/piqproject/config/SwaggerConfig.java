e6c6d70de954d5267c02988ca94d35346d1f0856
ttkakrnl33 <ttkakrnl33@gmail,com>
2025-08-09 00:49:33 +0900

    Swagger 추가 2

--- src/main/java/piq/piqproject/config/SwaggerConfig.java
+++ src/main/java/piq/piqproject/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package piq.piqproject.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("piq project API")
+                        .version("1.0.0")
+                        .description("API 목록"));
+    }
 }
