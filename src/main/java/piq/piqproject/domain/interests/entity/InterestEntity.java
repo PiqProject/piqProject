33e39e6e026191aae2c2fc80e6cbe635c6ed4dd4
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-10-01 15:17:21 +0900

    chore(interest): 초기 관심사 데이터 추가
    - 개발 환경에서의 테스트 및 기능 확인을 위해 data.sql을 사용하여 관심사 초기 데이터를 추가합니다.
    - 향후 데이터베이스 마이그레이션 도구(Flyway/Liquibase)를 도입할 때 정식 스크립트로 전환하여 관리할 예정입니다.

---
+++ src/main/java/piq/piqproject/domain/interests/entity/InterestEntity.java
@@ -0,0 +1,36 @@
+package piq.piqproject.domain.interests.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "interests")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InterestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String keyword;
+
+    @Builder
+    private InterestEntity (String keyword) {
+        this.keyword = keyword;
+    }
+
+    public static InterestEntity of (String keyword) {
+        return InterestEntity.builder()
+                .keyword(keyword)
+                .build();
+    }
+}
